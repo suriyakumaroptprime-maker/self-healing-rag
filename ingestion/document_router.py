dba0f837db40b8c77825ff472a88cda6b7f0c6ab
suriya S <suriyakumar.optprime@gmail.com>
2026-06-14 20:22:43 +0530

    Disable OCR for deployment

--- ingestion/document_router.py
+++ ingestion/document_router.py
@@ -1,7 +1,14 @@
 from ingestion.pdf_loader import PDFLoader
 from ingestion.csv_loader import CSVLoader
 from ingestion.excel_loader import ExcelLoader
-from ingestion.image_loader import ImageLoader
+
+try:
+
+    from ingestion.image_loader import ImageLoader
+
+except Exception:
+
+    ImageLoader = None
 
 
 class DocumentRouter:
@@ -14,7 +21,11 @@ class DocumentRouter:
 
         self.excel_loader = ExcelLoader()
 
-        self.image_loader = ImageLoader()
+        self.image_loader = None
+
+        if ImageLoader is not None:
+
+            self.image_loader = ImageLoader()
 
     def load_document(
         self,
@@ -52,6 +63,12 @@ class DocumentRouter:
             or
             file_path.endswith(".jpeg")
         ):
+
+            if self.image_loader is None:
+
+                raise ValueError(
+                    "OCR support is disabled in deployment."
+                )
 
             return self.image_loader.load(
                 file_path
